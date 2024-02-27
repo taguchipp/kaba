57ab0212c91851ef1ac90afb541ae89beffe8caa
ttaguchi <ozaki6626pyahoo.co.jp>
2024-02-27 14:22:38 +0900

    s7.pyとdownloadフォルダ作成

--- s7.py
+++ s7.py
@@ -1,15 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
+#フォルダ操作モジュール
+from pathlib import Path
+#url操作モジュール
+import urllib
+#時間操作モジュール
+import time
 
-res=requests.get('https://joytas.net/kaba/')
+load_url='https://joytas.net/kaba/'
+res=requests.get(load_url)
 res.encoding=res.apparent_encoding
 #第１引数にhtml文字列、第２引数にパーサーを指定する。今回は追加ライブラリ不要なhtml.parserを指定
 soup=BeautifulSoup(res.text,'html.parser')
 
-#li要素の中のaを全部取得
-links=soup.select('li a')
+#Pathオブジェクト作成
+out_folder=Path('download')
+
+out_folder.mkdir(exist_ok=True)
+
+#img要素を全部取得
+imgs=soup.select('img')
 
 #ファイル書き込み
-with open('zoo.txt','w',encoding='utf-8') as file:
-    for link in links:
-        file.write(f'{link.text}:{link.get("href")}\n')
+for img in imgs:
+    src=img.get('src')
+
+    #画像相対urlを絶対urlに変換
+    img_url = urllib.parse.urljoin(load_url,src)
+
+    #get通信で画像をロード
+    loaded_img=requests.get(img_url)
+
+    #ファイル名取得
+    file_name=img.get('src').split('/')[-1]
+
+    #保存画像パス
+    out_path=out_folder.joinpath(file_name)
+
+    #wbはバイナリ書き込み
+    with open(out_path,"wb") as file:
+        #contentはバイナリデータ
+        file.write(loaded_img.content)
+
+    #DOS攻撃にならないように１秒開ける
+    time.sleep(1)
