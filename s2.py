1d5fb10068f5e5ee66b4f8a1e23089b7f8df7a7e
ttaguchi <ozaki6626pyahoo.co.jp>
2024-02-27 13:22:43 +0900

    s2.py作成

---
+++ s2.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+res=requests.get('https://joytas.net/kaba/')
+res.encoding=res.apparent_encoding
+#第１引数にhtml文字列、第２引数にパーサーを指定する。今回は追加ライブラリ不要なhtml.parserを指定
+soup=BeautifulSoup(res.text,'html.parser')
+#BeautifulSoupオブジェクトをprintにそのまま渡すと全文を表示する
+#print(soup)
+
+#タグで要素取得
+ele=soup.find('title')#<title>要素
+#要素のtextコンテントを表示
+print(ele.text)
