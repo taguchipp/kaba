10f4cf4a28176eb07c8bb2c58690ff24fc81fcbb
ttaguchi <ozaki6626pyahoo.co.jp>
2024-02-27 13:42:52 +0900

    s4-2.py作成

---
+++ s4-2.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+res=requests.get('https://joytas.net/kaba/')
+res.encoding=res.apparent_encoding
+#第１引数にhtml文字列、第２引数にパーサーを指定する。今回は追加ライブラリ不要なhtml.parserを指定
+soup=BeautifulSoup(res.text,'html.parser')
+#BeautifulSoupオブジェクトをprintにそのまま渡すと全文を表示する
+#print(soup)
+
+#タグで要素取得
+ele=soup.find('title')#<title>要素
+#要素のtextコンテントを表示
+#print(ele.text)
+
+#要素を結果セットとして取得
+imgs=soup.find_all('img')
+#for img in imgs:
+    #属性にアクセスするにはgetメソッドを使う
+    #print(img.get('src'))
+
+#idを指定
+div=soup.find(id='headerImageBox')
+
+#classで取得
+imgs=soup.select('.headerImage')
+#for img in imgs:
+    #print(img.get('src'))
+
+tds = soup.select('tr td:first-child')
+for td in tds:
+    print(td.text)
